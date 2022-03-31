17d5942f6fe98366236166a60d2a05790c56356e
inrir <kimms5617@naver.com>
2022-03-31 18:57:29 +0900

    for문 중간까지 함

--- src/Step3_for.java
+++ src/Step3_for.java
@@ -6,20 +6,37 @@ import java.util.StringTokenizer;
 public class Step3_for {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int a = Integer.parseInt(bf.readLine());
-
-        for(int i = a; i>=1; i--){
-            System.out.println(i);
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        StringTokenizer stt = new StringTokenizer(bf.readLine());
+        int [] nums = new int[a];
+        int [] tre = new int[a];
+        int t = 0;
+        for(int i = 0; i<a; i++){
+            nums[i] = Integer.parseInt(stt.nextToken());
+            if(nums[i] < b){
+                tre[t] = nums[i];
+                t++;
+            }
         }
+        for(int i = 0; i<t; i++){
+            System.out.print(tre[i] + " ");
+        }
+
+
+        //        System.out.println("*".repeat(i)); // 문자열 갯수 곱가능
 
        /* 반복문 내부에서 값을 여러개 띄워쓰기로 받을 수 있는 방법
          for(int i = 0; i<a; i++){
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
+            num_b[i] = b;
+            num_c[i] = c;
             nums[i] = b+c;
-        }*/
+        }
+         */
 
     }
 }
